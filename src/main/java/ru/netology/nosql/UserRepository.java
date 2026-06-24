3c733aaed31f78d6f69878a67f5884bd241ed55c
zartem2001 <116199542+zartem2001@users.noreply.github.com>
2026-06-24 10:15:56 +0300

    -

--- src/main/java/ru/netology/nosql/UserRepository.java
+++ src/main/java/ru/netology/nosql/UserRepository.java
@@ -1,4 +1,14 @@
 package ru.netology.nosql;
 
-public interface UserRepository {
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+public interface UserRepository extends MongoRepository<User, String> {
+
+    List<User> findByName(String name);
+
+    List<User> findByAgeGreaterThan(int age);
+
+    List<User> findByEmailContaining(String domain);
 }
